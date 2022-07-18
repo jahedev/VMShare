ff4872c147ed54199f519b9038f1e62a4133a33d
Jahed Hossain <jahedev@gmail.com>
2022-07-18 13:17:46 -0400

    C++ Strings

--- cpp/projects/cpp_strings/cpp_strings.cpp
+++ cpp/projects/cpp_strings/cpp_strings.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int main()
 {
-    string s1 = "Jahed";
+    string first_name = "Jahed";
+    string last_name {"Hossain"};
+    string full_name = first_name + " " + last_name;
 
-    cout << "Hello, " << s1 << endl; 
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    string subset {alphabet, 0, 4};
+
+    cout << "Hello, " << full_name << endl; 
+    cout << "Character at index 0: " << full_name.at(0) << endl; 
+    cout << "Jahed < jahed: " << (first_name < "jahed") << endl; 
+    cout << "full_name.find('x'): " << full_name.find('h') << endl; 
+    cout << "Alphabet? , " << subset << endl; 
     
     return 0;
 }
